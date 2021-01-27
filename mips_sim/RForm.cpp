867a13e148391e2d8ee6ce21b47fad3da5bfc496
pettyac <76820120+pettyac@users.noreply.github.com>
2021-01-27 11:42:41 -0600

    Add files via upload

--- mips_sim/RForm.cpp
+++ mips_sim/RForm.cpp
@@ -3,8 +3,8 @@
 
 #include "RForm.h"
 
-RForm::RForm(std::string in, std::string pseudo, int func, RegFile & r)
-    : Inst(in, pseudo, 'R')
+RForm::RForm(std::string in, int func, RegFile & r)
+    : Inst(in, 'R')
 {
     int reg_number[3];      //temp variable for register number
     int i = 0;              //iterates over pseudo inst
@@ -13,12 +13,12 @@ RForm::RForm(std::string in, std::string pseudo, int func, RegFile & r)
 
     for (int j = 0; j < 3; ++j)         // find 3 register names in pseudo code
     {
-        while (pseudo[i] != '$') ++i;   //skip over whitespace and commas
+        while (in[i] != '$') ++i;   //skip over whitespace and commas
         
         reg_name = "";
         for (int k = 0; k < 3; ++k)
         {
-            reg_name += pseudo[i];
+            reg_name += in[i];
             ++i;
         }
         reg_number[j] = r.find_in_map(reg_name);    //look up register name & find assoc. integer
@@ -44,13 +44,26 @@ RForm::RForm(std::string in, std::string pseudo, int func, RegFile & r)
 }
 
 
-void RForm::dec_form()
+RForm::RForm(std::string in, int funct, int rd, int rs, int rt)
+    : Inst(in, 'R')
 {
-    std::cout << "< ";
+    std::cout << in << std::endl;    
+    decimal_form[0] = 0;
+    decimal_form[1] = rs;
+    decimal_form[2] = rt;
+    decimal_form[3] = rd;
+    decimal_form[4] = 0;
+    decimal_form[5] = funct;
     
-    for (int i = 0; i < 6; ++i)
-    {
-        std::cout << decimal_form[i] << " ";
-    }
-    std::cout << ">";
+    /* converts the integers into a binary rep of the inst */
+    std::string bin_rep;
+
+    bin_rep = std::bitset<OPCODE_LENGTH> (OPCODE()).to_string();
+    bin_rep += std::bitset<REG_LENGTH> (RS()).to_string();
+    bin_rep += std::bitset<REG_LENGTH> (RT()).to_string();
+    bin_rep += std::bitset<REG_LENGTH> (RD()).to_string();
+    bin_rep += std::bitset<SHAMT_LENGTH> (SHAMT()).to_string();
+    bin_rep += std::bitset<FUNCT_LENGTH> (FUNCT()).to_string();
+    
+    set_machine_code(bin_rep);
 }
